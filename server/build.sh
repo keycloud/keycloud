c128fb07d1fc63ae9648cae92e8327e38f56cdf0
Dudeldu <mustermann.informatik@gmail.com>
2019-10-29 11:43:56 +0100

    Remove building twice
    If go get doesn't exit with an error, the server would be build twice

--- server/build.sh
+++ server/build.sh
@@ -1,5 +1,5 @@
 path=`pwd`
 echo ${path}
-sudo -i -- sh -c "cd ${path} && export GOPATH=${path} && go get . && go build ."
+sudo -i -- sh -c "cd ${path} && export GOPATH=${path} && go get ."
 sudo -i -- sh -c "cd ${path} && export GOPATH=${path} && go build -o server ."
 echo "END"
