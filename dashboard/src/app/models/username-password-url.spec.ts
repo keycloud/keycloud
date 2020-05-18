a9c36e21c88b3fdfbcb9d3521c4122a2312482b0
hottek <dev@mardybum.de>
2020-05-18 08:55:42 +0200

    Alter model

--- dashboard/src/app/models/username-password-url.spec.ts
+++ dashboard/src/app/models/username-password-url.spec.ts
@@ -2,6 +2,6 @@ import { UsernamePasswordUrl } from './username-password-url';
 
 describe('UsernamePasswordUrl', () => {
   it('should create an instance', () => {
-    expect(new UsernamePasswordUrl()).toBeTruthy();
+    expect(new UsernamePasswordUrl('', '', '', '', '')).toBeTruthy();
   });
 });
