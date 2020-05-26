a377336a1835bb79c80e92d41a86c38633107b32
hottek <dev@mardybum.de>
2020-05-26 11:12:36 +0200

    Fix for 401 messages, fix masterpassword

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent} from './login/login.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {SettingsComponent} from './settings/settings.component';
-import {AuthGuard} from './util/auth.guard';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: '*', redirectTo: '' }
 ];
 
