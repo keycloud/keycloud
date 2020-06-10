ffa11f4e9582f5f6e1a3afde2596e0b6a5697da8
hottek <dev@mardybum.de>
2020-06-10 13:15:52 +0200

    Add new karma tests
    Need to discuss if these are expanded

--- dashboard/src/app/services/user.service.spec.ts
+++ dashboard/src/app/services/user.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {UserRegister} from '../models/user-register';
+import {User} from '../models/user';
 
 describe('UserService', () => {
   let service: UserService;
@@ -11,9 +13,28 @@ describe('UserService', () => {
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(UserService);
+    spyOn(service, 'getUser');
+    spyOn(service, 'register');
+    spyOn(service, 'login');
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should call getUser()', () => {
+    service.getUser();
+    expect(service.getUser).toHaveBeenCalled();
+  });
+
+  it('should call register()', () => {
+    service.register(new UserRegister('test', 'a@b.c'));
+    expect(service.register).toHaveBeenCalledWith(new UserRegister('test', 'a@b.c'));
+  });
+
+  it('should call login()', () => {
+    service.login(new User('test', 'asdf'));
+    expect(service.login).toHaveBeenCalledWith(new User('test', 'asdf'));
+  });
+
 });
