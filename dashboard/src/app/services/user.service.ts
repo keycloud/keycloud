5731db60774f9045ce52af870d16b81058d7c0f4
hottek <dev@mardybum.de>
2020-06-23 16:51:16 +0200

    Construct request url in http intercepter

--- dashboard/src/app/services/user.service.ts
+++ dashboard/src/app/services/user.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {environment} from '../../environments/environment.prod';
 import {User} from '../models/user';
 import {UserRegister} from '../models/user-register';
 
@@ -15,44 +14,42 @@ export class UserService {
     responseType: 'text' as 'json'
   };
 
-  apiUrl = environment.apiUrl;
-
   constructor(
     private httpClient: HttpClient
   ) {
   }
 
   getUser(): Observable<any> {
-    return this.httpClient.get<User>(`${this.apiUrl}/user`, this.httpOptions);
+    return this.httpClient.get<User>(`/user`, this.httpOptions);
   }
 
   register(body: UserRegister): Observable<any> {
-    return this.httpClient.post<UserRegister>(`${this.apiUrl}/standard/register`, JSON.stringify(body),
+    return this.httpClient.post<UserRegister>(`/standard/register`, JSON.stringify(body),
       this.httpOptions);
   }
 
   login(body: User): Observable<any> {
-    return this.httpClient.post<User>(`${this.apiUrl}/standard/login`, JSON.stringify(body),
+    return this.httpClient.post<User>(`/standard/login`, JSON.stringify(body),
       this.httpOptions);
   }
 
   logout(): Observable<any> {
-    return this.httpClient.post(`${this.apiUrl}/logout`, '', this.httpOptions);
+    return this.httpClient.post(`/logout`, '', this.httpOptions);
   }
 
   webauthnRegistrationStart(body: UserRegister): Observable<any> {
-    return this.httpClient.post<UserRegister>(`${this.apiUrl}/webauthn/registration/start`, JSON.stringify(body), this.httpOptions);
+    return this.httpClient.post<UserRegister>(`/webauthn/registration/start`, JSON.stringify(body), this.httpOptions);
   }
 
   webauthnRegistrationFinish(body: any): Observable<any> {
-    return this.httpClient.post(`${this.apiUrl}/webauthn/registration/finish`, JSON.stringify(body), this.httpOptions);
+    return this.httpClient.post(`/webauthn/registration/finish`, JSON.stringify(body), this.httpOptions);
   }
 
   webauthnLoginStart(body: UserRegister): Observable<any> {
-    return this.httpClient.post<UserRegister>(`${this.apiUrl}/webauthn/login/start`, JSON.stringify(body), this.httpOptions);
+    return this.httpClient.post<UserRegister>(`/webauthn/login/start`, JSON.stringify(body), this.httpOptions);
   }
 
   webauthnLoginFinish(body: any): Observable<any> {
-    return this.httpClient.post(`${this.apiUrl}/webauthn/login/finish`, JSON.stringify(body), this.httpOptions);
+    return this.httpClient.post(`/webauthn/login/finish`, JSON.stringify(body), this.httpOptions);
   }
 }
--- dashboard/src/environments/environment.ts
+++ dashboard/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: location.host
+  apiUrl: `${location.hostname}`,
 };
 
 /*
